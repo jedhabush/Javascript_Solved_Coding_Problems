ce635a202dcf4a7f7e979e78fd836c365a8dda53
Jed Habush <90814324+jedhabush@users.noreply.github.com>
2022-11-02 16:29:22 +1100

    Update app.js

--- app.js
+++ app.js
@@ -336,3 +336,52 @@ const numbersToWords = (number) => {
 console.log(numbersToWords(8003));
 
 
+//--------------------- Problem 10 ----------------------------
+
+
+/*
+Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) 
+that checks whether the two arrays have the "same" elements,
+ with the same multiplicities (the multiplicity of a member is the number of times it appears). 
+ "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+
+Remarks
+a or b might be [] or {} (all languages except R, Shell).
+a or b might be nil or null or None or nothing 
+(except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+*/
+
+const comp = (arr1, arr2) => {
+  if (arr1 === null || arr2 === null) {
+    return false;
+  }
+
+  let arr1Sorted = arr1.sort((a, b) => b - a).map((num) => num * num);
+  let arr2Sorted = arr2.sort((a, b) => b - a);
+  let resultTrue = 0;
+  let resultFalse = 0;
+  for (let i = 0; i < arr1Sorted.length; i++) {
+    if (arr1Sorted[i] === arr2Sorted[i]) {
+      resultTrue++;
+    } else {
+      resultFalse++;
+    }
+  }
+
+  //if the counter of true's equal the number of array 1 length, it means all of the elements are true
+  return resultTrue === arr1Sorted.length;
+};
+
+console.log(
+  comp(
+    [121, 144, 19, 161, 19, 144, 19, 11],
+    [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+  )
+);
