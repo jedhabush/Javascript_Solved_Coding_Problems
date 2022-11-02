a7adbbf8a12d2390e3db5d836d6d4c9898c2c759
Jed Habush <90814324+jedhabush@users.noreply.github.com>
2022-11-02 15:17:47 +1100

    Update app.js

--- app.js
+++ app.js
@@ -242,3 +242,97 @@ const encryption = (text) => {
 
 console.log(encryption("Thank you Piotr for all your help")); 
 
+
+//--------------------- Problem 9 ----------------------------
+/*
+reate a function that transforms any positive number to a string representing the number in words. The function should work for all numbers between 0 and 999999.
+---------
+EXAMPLES:
+number2words(0)  ==>  "zero"
+number2words(1)  ==>  "one"
+number2words(9)  ==>  "nine"
+number2words(10)  ==>  "ten"
+number2words(17)  ==>  "seventeen"
+number2words(20)  ==>  "twenty"
+number2words(21)  ==>  "twenty-one"
+number2words(45)  ==>  "forty-five"
+number2words(80)  ==>  "eighty"
+number2words(99)  ==>  "ninety-nine"
+number2words(100)  ==>  "one hundred"
+number2words(301)  ==>  "three hundred one"
+number2words(799)  ==>  "seven hundred ninety-nine"
+number2words(800)  ==>  "eight hundred"
+number2words(950)  ==>  "nine hundred fifty"
+number2words(1000)  ==>  "one thousand"
+number2words(1002)  ==>  "one thousand two"
+number2words(3051)  ==>  "three thousand fifty-one"
+number2words(7200)  ==>  "seven thousand two hundred"
+number2words(7219)  ==>  "seven thousand two hundred nineteen"
+number2words(8330)  ==>  "eight thousand three hundred thirty"
+number2words(99999)  ==>  "ninety-nine thousand nine hundred ninety-nine"
+number2words(888888)  ==>  "eight hundred eighty-eight thousand eight hundred eighty-eight"
+  */
+
+const numbersToWords = (number) => {
+  let numbersA = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+  ];
+
+  let numbersB = [
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+  ];
+
+  if (number <= 19 && number >= 0) {
+    return numbersA[number];
+  }
+  if (number >= 20 && number <= 99) {
+    return (
+      numbersB[Math.floor(number / 10) - 2] +
+      (number % 10 === 0 ? "" : "-" + numbersA[number % 10])
+    );
+  }
+
+  if (number >= 100 && number <= 999) {
+    return (
+      numbersA[Math.floor(number / 100)] +
+      " hundred" +
+      (number % 100 === 0 ? "" : "-" + numbersToWords(number % 100))
+    );
+  } else {
+    return (
+      numbersToWords(Math.floor(number / 1000)) +
+      " thousand" +
+      (number % 1000 === 0 ? "" : " " + numbersToWords(number % 1000))
+    );
+  }
+};
+
+console.log(numbersToWords(8003));
+
+
