64012e5772b47b000b475d17aa1d891b81143b6b
Jed Habush <90814324+jedhabush@users.noreply.github.com>
2022-11-02 16:32:46 +1100

    Update app.js

--- app.js
+++ app.js
@@ -345,6 +345,13 @@ that checks whether the two arrays have the "same" elements,
  with the same multiplicities (the multiplicity of a member is the number of times it appears). 
  "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
 
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 
+ 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+
+
 Examples
 Valid arrays
 a = [121, 144, 19, 161, 19, 144, 19, 11]  
